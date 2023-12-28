aa5bcad2e9467a5001d1badfeae3d358036b569b
Cade Gray <47758856+cade-gray@users.noreply.github.com>
2023-12-27 23:12:57 -0500

    Created first draft of grid

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import React from "react";
 interface Joke {
   setup: string;
   punchline: string;
+  formattedPunchline?: string;
 }
 function App() {
+  const testPunchline = "Horses will no longer   get a square meal.";
   const [joke, setJoke] = React.useState<Joke>({
     setup: "",
     punchline: "",
+    formattedPunchline: "",
   });
   useEffect(() => {
     fetch("https://api.cadegray.dev/joke")
@@ -19,12 +22,23 @@ function App() {
         setJoke({ setup: jokeBody.setup, punchline: jokeBody.punchline });
       });
   }, []);
+
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center max-w-screen-lg mx-auto">
       <h1 className="text-3xl font-bold underline">Jokedle</h1>
       <p className="text-xl font-semibold">{joke.setup}</p>
-      <p className="text-xl font-semibold">{joke.punchline}</p>
-      <div></div>
+      <div className="grid grid-cols-12 grid-rows-6">
+        {Array.from(testPunchline, (char, index) => (
+          <div
+            key={index}
+            className={`m-1 p-1 flex items-center justify-center text-2xl font-semibold ${
+              char === " " ? "bg-gray-400/20" : "bg-gray-300"
+            }`}
+          >
+            {char === " " ? "" : char}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
